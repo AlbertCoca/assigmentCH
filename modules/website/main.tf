172f9530830ddb5d48d29dc84e12f4414433d35c
AlbertCoca <albert.coca@mitigasolutions.com>
2023-06-18 18:33:06 +0200

    issue#2: Add dns record to the website bucket

--- modules/website/main.tf
+++ modules/website/main.tf
@@ -28,3 +28,33 @@ resource "aws_s3_object" "error" {
   content_type = "text/html"
   source       = "${path.module}/website/error.html"
 }
+
+# In this case we are creating a vpc and a private route53 zone for testing
+# proposes but in a real environment the configurations would be much different
+# assesing the need for the different domains.
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_route53_zone" "private" {
+  name = var.domain
+
+  vpc {
+    vpc_id = aws_vpc.main.id
+  }
+}
+
+data "aws_s3_bucket" "default" {
+  bucket = var.s3_bucket_id
+}
+
+resource "aws_route53_record" "web_mycardo_com" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = var.dns_record_name
+  type    = "A"
+  alias {
+    name                   = aws_s3_bucket_website_configuration.default.website_endpoint
+    zone_id                = data.aws_s3_bucket.default.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
