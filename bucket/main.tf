1379351a80f404b866fe02138c880362d59ba320
AlbertCoca <albert.coca@mitigasolutions.com>
2023-06-18 18:23:25 +0200

    issue#1: Make the bucket private

--- bucket/main.tf
+++ bucket/main.tf
@@ -12,3 +12,17 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
     }
   }
 }
+
+resource "aws_s3_bucket_ownership_controls" "default" {
+  bucket = aws_s3_bucket.default.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "default" {
+  depends_on = [aws_s3_bucket_ownership_controls.default]
+
+  bucket = aws_s3_bucket.default.id
+  acl    = "private"
+}
